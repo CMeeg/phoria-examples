951855b539e5d543b8f6dad35296a4fd87b9a1dd
Chris Meagher <cpmeeg@gmail.com>
2025-01-10 18:56:36 +0000

    Fix type narrowing issues in server entry

--- examples/getting-started/ui/src/entry-server.ts
+++ examples/getting-started/ui/src/entry-server.ts
@@ -3,8 +3,6 @@ import "./components/register"
 import type { PhoriaIsland } from "@phoria/phoria/server"
 
 async function renderPhoriaIsland(island: PhoriaIsland) {
-  // TODO: Need to deal with the `any` here
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 	return await island.render()
 }
 
--- examples/with-styled-components/WebApp/ui/src/entry-server.ts
+++ examples/with-styled-components/WebApp/ui/src/entry-server.ts
@@ -1,17 +1,16 @@
-import "@phoria/phoria-react/server"
-import "./components/register"
-import type { PhoriaIsland } from "@phoria/phoria/server"
-import { renderWithStyledComponents } from "./server/ssr"
+import { isReactIsland } from "@phoria/phoria-react/server";
+import "./components/register";
+import type { PhoriaIsland } from "@phoria/phoria/server";
+import { renderWithStyledComponents } from "./server/ssr";
 
 async function renderPhoriaIsland(island: PhoriaIsland) {
-	switch (island.framework) {
-		case "react":
-			return await island.render({
-				renderComponent: renderWithStyledComponents
-			})
-		default:
-			return await island.render()
+	if (isReactIsland(island)) {
+		return await island.render({
+			renderComponent: renderWithStyledComponents
+		})
 	}
+
+	return await island.render();
 }
 
-export { renderPhoriaIsland }
+export { renderPhoriaIsland };
