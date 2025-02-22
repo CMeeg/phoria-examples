538489a908bd5d3b77fbd4ce2c56a48d3ed4b577
Chris Meagher <cpmeeg@gmail.com>
2025-02-22 17:16:15 +0000

    Update with-styled-components example

--- examples/with-styled-components/vite.server.config.ts
+++ examples/with-styled-components/vite.server.config.ts
@@ -3,25 +3,24 @@ import { parsePhoriaAppSettings } from "@phoria/phoria/server"
 import { type UserConfig, defineConfig } from "vite"
 
 export default defineConfig(async () => {
-	const dotnetEnv = process.env.DOTNET_ENVIRONMENT ?? process.env.ASPNETCORE_ENVIRONMENT ?? "Development"
-	const appsettings = await parsePhoriaAppSettings({
-		environment: dotnetEnv,
-		cwd: join(process.cwd(), "WebApp")
-	})
+  const dotnetEnv = process.env.DOTNET_ENVIRONMENT ?? process.env.ASPNETCORE_ENVIRONMENT ?? "Development"
+  const appsettings = await parsePhoriaAppSettings({
+    environment: dotnetEnv,
+    cwd: join(process.cwd(), "WebApp")
+  })
 
-	// https://vite.dev/config/
-	return {
-		root: appsettings.root,
-		base: appsettings.base,
-		build: {
-			ssr: true,
-			target: "es2022",
-			copyPublicDir: false,
-			emptyOutDir: true,
-			outDir: `${appsettings.build.outDir}/server`,
-			rollupOptions: {
-				input: `${appsettings.root}/src/server.ts`
-			}
-		}
-	} satisfies UserConfig
+  return {
+    root: appsettings.root,
+    base: appsettings.base,
+    build: {
+      ssr: true,
+      target: "es2022",
+      copyPublicDir: false,
+      emptyOutDir: true,
+      outDir: `${appsettings.build.outDir}/server`,
+      rollupOptions: {
+        input: `${appsettings.root}/src/server.ts`
+      }
+    }
+  } satisfies UserConfig
 })
